98105df49abde05f7e4442139b1992dc792b0f1a
Tomasz Wostal <tomasz.wostal@hemolens.eu>
2024-07-11 12:48:33 +0200

    add brewfile

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.buildEnv {
-          name = "hello";
-          paths = with pkgs; [
-            hello
-          ];
-        };
-      }
-    );
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+  outputs = { self, nixpkgs }: {
+    devShell.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.aarch64-darwin; [
+        cowsay
+        ruby
+      ];
+    };
+  };
 }
+# {
+#   inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+#   inputs.flake-utils.url = "github:numtide/flake-utils";
+#
+#   outputs = { self, nixpkgs, flake-utils }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs = import nixpkgs { inherit system; };
+#       in
+#       {
+#         packages.default = pkgs.buildEnv {
+#           name = "hello";
+#           paths = with pkgs; [
+#             hello
+#           ];
+#         };
+#       }
+#     );
+# }
